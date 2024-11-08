254e199ac2ba0200de81b4091ec628c4f190c37b
AkshatSemwall <153000862+AkshatSemwall@users.noreply.github.com>
2024-11-08 15:35:20 +0530

    Create 21.cpp

---
+++ 21.cpp
@@ -0,0 +1,240 @@
+#include <iostream>
+using namespace std;
+
+class A {
+public:
+    void display1() {
+        cout<< "Class A"<<endl;
+    }
+};
+
+class B:virtual public A {
+public:
+    void display2() {
+        cout <<"Class B"<< endl;
+    }
+};
+
+class C:virtual public A {
+public:
+    void display3() {
+        cout << "Class C" << endl;
+    }
+};
+
+class D:public B,public C {
+public:
+    void display4() {
+        cout << "Class D" << endl;
+    }
+};
+
+
+int main() {
+    D obj;
+    obj.display1(); #include <iostream>
+using namespace std;
+
+class A {
+public:
+    void display1() {
+        cout<< "Class A"<<endl;
+    }
+};
+
+class B:virtual public A {
+public:
+    void display2() {
+        cout <<"Class B"<< endl;
+    }
+};#include <iostream>
+using namespace std;
+
+class A {
+public:
+    void display1() {
+        cout<< "Class A"<<endl;
+    }
+};
+
+class B:virtual public A {
+public:
+    void display2() {
+        cout <<"Class B"<< endl;
+    }
+};
+
+class C:virtual public A {
+public:
+    void display3() {
+        cout << "Class C" << endl;
+    }
+};
+
+class D:public B,public C {
+public:#include <iostream>
+using namespace std;
+
+class A {
+public:
+    void display1() {
+        cout<< "Class A"<<endl;
+    }
+};
+
+class B:virtual public A {
+public:
+    void display2() {
+        cout <<"Class B"<< endl;
+    }
+};
+
+class C:virtual public A {
+public:
+    void display3() {
+        cout << "Class C" << endl;
+    }
+};
+
+class D:public B,public C {
+public:
+    void display4() {
+        cout << "Class D" << endl;
+    }#include <iostream>
+using namespace std;
+
+class A {
+public:
+    void display1() {
+        cout<< "Class A"<<endl;
+    }
+};
+
+class B:virtual public A {
+public:
+    void display2() {
+        cout <<"Class B"<< endl;
+    }
+};
+
+class C:virtual public A {
+public:
+    void display3() {
+        cout << "Class C" << endl;
+    }
+};
+
+class D:public B,public C {#include <iostream>
+using namespace std;
+
+class A {
+public:
+    void display1() {
+        cout<< "Class A"<<endl;
+    }
+};
+
+class B:virtual public A {
+public:
+    void display2() {
+        cout <<"Class B"<< endl;
+    }
+};
+
+class C:virtual public A {
+public:
+    void display3() {
+        cout << "Class C" << endl;
+    }
+};
+
+class D:public B,public C {
+public:
+    void display4() {
+        cout << "Class D" << endl;
+    }
+};
+
+
+int main() {
+    D obj;
+    obj.display1(); 
+
+
+    return 0;
+}
+
+
+public:
+    void display4() {
+        cout << "Class D" << endl;
+    }
+};
+
+
+int main() {
+    D obj;
+    obj.display1(); 
+
+
+    return 0;
+}
+
+
+};
+
+
+int main() {
+    D obj;
+    obj.display1(); 
+
+
+    return 0;
+}
+
+
+    void display4() {
+        cout << "Class D" << endl;
+    }
+};
+
+
+int main() {
+    D obj;
+    obj.display1(); 
+
+
+    return 0;
+}
+
+
+
+class C:virtual public A {
+public:
+    void display3() {
+        cout << "Class C" << endl;
+    }
+};
+
+class D:public B,public C {
+public:
+    void display4() {
+        cout << "Class D" << endl;
+    }
+};
+
+
+int main() {
+    D obj;
+    obj.display1(); 
+
+
+    return 0;
+}
+
+
+
+
+    return 0;
+}
+
